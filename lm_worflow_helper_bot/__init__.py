fff514458488179d184941999d199c074d8e4a85
Smarandii <olegsmarandi@gmail.com>
2022-11-30 13:18:47 +0300

    directory bug fix

--- lm_worflow_helper_bot/__init__.py
+++ lm_worflow_helper_bot/__init__.py
@@ -3,6 +3,13 @@ from telebot import TeleBot
 import ics
 import codecs
 import datetime
+try:
+    os.mkdir('calendars')
+    print("Calendars dir created!")
+except:
+    print("Calendars dir existed!")
+
 
 os.environ['TG_API_BOT'] = '5825095498:AAFUbxz_7cexnzCvzJl-F3-3J7JF2ARKSdA'
 bot = TeleBot(os.environ['TG_API_BOT'])
+print("TeleBot is online!\n")
